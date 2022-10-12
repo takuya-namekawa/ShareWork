8ed48a23cf0b11fca15feea84ae5acc4615a8e3a
takuya-namekawa <tatatatatata89@gmail.com>
2022-10-12 05:10:22 +0000

    management edit作成ok

--- app/controllers/management/managements_controller.rb
+++ app/controllers/management/managements_controller.rb
@@ -3,7 +3,20 @@ class Management::ManagementsController < ApplicationController
     @management = current_management
   end
 
+  def update
+    @management = current_management
+    @management.update(management_params)
+    redirect_to my_page_management_managements_path
+  end
+
   def edit
     @management = current_management
   end
+
+  private
+
+  def management_params
+    params.require(:management).permit(:name, :email, :profile_image, :job)
+  end
+
 end
