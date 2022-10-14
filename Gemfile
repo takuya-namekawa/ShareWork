39d1b692f82ba23c2709373a4e27fe37097671f6
takuya-namekawa <tatatatatata89@gmail.com>
2022-10-14 03:55:21 +0000

    christmas sort

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'mini_magick'
 gem 'image_processing'
+gem 'kaminari','~> 1.2.1'
--- app/controllers/shop/christmases_controller.rb
+++ app/controllers/shop/christmases_controller.rb
@@ -1,10 +1,20 @@
 class Shop::ChristmasesController < ApplicationController
   def index
-    @christmases = Christmas.all
+
     @christmas = Christmas.new
     @shop_total_amount = 0
     @shop_tptal_booking_target = 0
     @shop_store_sales_amount = 0
+    # モデル名.order(カラム名: "DESC")
+    @current_year = params[:current_year].present? ? params[:current_year] : Time.zone.now.year
+    @all_christmases = Christmas.all
+    @christmases = @all_christmases.where(year: @current_year)
+    @christmases = @christmases.order(booking_amount: :asc) if params[:sort_amount] == "booking_amount_asc"
+    @christmases = @christmases.order(total_amount: :asc) if params[:sort_amount] == "total_amount_asc"
+    @christmases = @christmases.order(booking_amount: :desc) if params[:sort_amount] == "booking_amount_desc"
+    @christmases = @christmases.order(total_amount: :desc) if params[:sort_amount] == "total_amount_desc"
+    @christmases = @christmases.page(params[:page])
+
   end
 
   def create
