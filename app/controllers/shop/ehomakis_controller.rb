8cbbc5bf8f88b4ef8cd9cd982724b02b4d5f753f
takuya-namekawa <tatatatatata89@gmail.com>
2022-10-14 05:24:32 +0000

    ehomaki sort

--- app/controllers/shop/ehomakis_controller.rb
+++ app/controllers/shop/ehomakis_controller.rb
@@ -1,10 +1,19 @@
 class Shop::EhomakisController < ApplicationController
   def index
-    @ehomakis = Ehomaki.all
+   
     @ehomaki = Ehomaki.new
     @shop_total_amount = 0
     @shop_tptal_booking_target = 0
     @shop_store_sales_amount = 0
+    
+    @current_year = params[:current_year].present? ? params[:current_year] : Time.zone.now.year
+    @all_ehomakis = Ehomaki.all
+    @ehomakis = @all_ehomakis.where(year: @current_year)
+    @ehomakis = @ehomakis.order(booking_amount: :asc) if params[:sort_amount] == "booking_amount_asc"
+    @ehomakis = @ehomakis.order(total_amount: :asc) if params[:sort_amount] == "total_amount_asc"
+    @ehomakis = @ehomakis.order(booking_amount: :desc) if params[:sort_amount] == "booking_amount_desc"
+    @ehomakis = @ehomakis.order(total_amount: :desc) if params[:sort_amount] == "total_amount_desc"
+    @ehomakis = @ehomakis.page(params[:page])
   end
 
   def create
