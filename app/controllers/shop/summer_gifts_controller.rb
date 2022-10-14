fa819ea6a354788d2fcbb564c44280a505f9e78e
takuya-namekawa <tatatatatata89@gmail.com>
2022-10-14 05:37:53 +0000

    summer_gifr sort

--- app/controllers/shop/summer_gifts_controller.rb
+++ app/controllers/shop/summer_gifts_controller.rb
@@ -1,11 +1,20 @@
 class Shop::SummerGiftsController < ApplicationController
   def index
-    @summer_gifts = SummerGift.all
+
     @summer_gift = SummerGift.new
     @shop_total_target_number = 0
     @shop_tptal_target_amount = 0
     @shop_total_number = 0
     @shop_total_amount = 0
+
+    @current_year = params[:current_year].present? ? params[:current_year] : Time.zone.now.year
+    @all_summer_gifts = SummerGift.all
+    @summer_gifts = @all_summer_gifts.where(year: @current_year)
+    @summer_gifts = @summer_gifts.order(number: :asc) if params[:sort_amount] == "number_asc"
+    @summer_gifts = @summer_gifts.order(total_amount: :asc) if params[:sort_amount] == "total_amount_asc"
+    @summer_gifts = @summer_gifts.order(number: :desc) if params[:sort_amount] == "number_desc"
+    @summer_gifts = @summer_gifts.order(total_amount: :desc) if params[:sort_amount] == "total_amount_desc"
+    @summer_gifts = @summer_gifts.page(params[:page])
   end
 
   def create
